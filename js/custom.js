c0f905b9f0810bbf083888738d2124f204407838
Emanuele Paolini <emanuele.paolini@unipi.it>
2023-01-10 09:18:27 +0000

    cliccando sulle intestazioni delle tabelle si fa un ordinamento

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,33 @@
+
+function add_sort_capability_to_tables($) {
+  console.log("HI THERE!");
+  var headers = document.getElementsByTagName("th");
+  for (var i=0; i<headers.length; ++i) {
+    headers[i].onclick = function() {
+      console.log(this.innerHTML);
+      var col_index = Array.prototype.indexOf.call(this.parentElement.children, this);
+      console.log(col_index);
+      var table = this.closest('table');
+      console.log(table);
+      var sorted = false;
+      while(!sorted) {
+        sorted = true;
+        for (var row_index=1; row_index < table.rows.length - 1; row_index++) {
+          var row0 = table.rows[row_index];
+          var row1 = table.rows[row_index+1];
+          var text0 = row0.getElementsByTagName("td")[col_index];
+          var text1 = row1.getElementsByTagName("td")[col_index];
+          if (text0.innerHTML.toLowerCase() > text1.innerHTML.toLowerCase()) {
+	    sorted = false;
+            row0.parentNode.insertBefore(row1, row0);
+          }
+        }
+      }
+    }
+  }
+}
+
+
 (function ( $ ) {
 	'use strict';
 
@@ -29,6 +59,8 @@
 		    }
 		  });
 		}
+
+	      add_sort_capability_to_tables($);
 	} );
 
 })( jQuery );
